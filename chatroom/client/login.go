8b61a43c5ca34177c4e49f80059e431fe687d1c9
linjunhan <m15521093428@aliyun.com>
2019-11-18 18:35:21 +0800

    完成了发送消息长度代码但是还没有测试

--- chatroom/client/login.go
+++ chatroom/client/login.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net"
 	"encoding/binary"
-	"go_code/chatroom/common/message"
+	"go_project/chatroom/common/message"
 )
 
 // 写一个登陆函数
@@ -17,5 +17,47 @@ func login(userId int, userPwd string) (err error) {
 		fmt.Println("net dial err ", err)
 		return 
 	}
-	defer conn
+	// 延时关闭
+	defer conn.Close()
+
+	// 2. 准备通过conn发送消息给服务
+	var mes message.Message
+	mes.Type = message.LoginMesType
+	// 3.创建了一个LoginMes 结构体
+	var loginMes message.LoginMes
+	loginMes.UserId = userId
+	login.UserPwd = userPwd
+
+	// 4.将loginMes 序列化
+	data, err := json.Marshall(loginMes)
+	if err != nil {
+		fmt.Println("json marshal err ", err)
+		return
+	}
+
+	// 5.把data赋给mes.Data字段
+	mes.Data = string(data)
+
+	// 6.将mes进行序列化
+	data, err = json.Marshall(mes)
+	if err != nil {
+		fmt.Println("mes json marshall err ", err)
+		return
+	}
+
+	// 7. 已经拿到了要送的data
+	// 7.1 先把data长度发送给服务器
+	// 先获取data的长度 -》 转成一个表示长度的bytes的切片
+	var pkgLen uint32
+	pkgLen = uint32(len(data))
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[0:4], pkgLen)
+	// 发送长度
+	n, err := conn.Write(buf[:4])
+	if n != 4 || err != nil {
+		fmt.Println("conn write(len) err ", err)
+		return 
+	}
+
+	fmt.Printf("客户端发送消息的长度 %d, 内容 %s", len(data), string(data))
 }
